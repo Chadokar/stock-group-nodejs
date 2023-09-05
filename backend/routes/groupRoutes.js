7afd9c5ce1b33b10fe1cde0ed1ac19913bf8a7c4
Shubham <shubhamchadokar36@gmail.com>
2023-09-05 22:11:17 +0530

    groupRoutes updated

--- backend/routes/groupRoutes.js
+++ backend/routes/groupRoutes.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
 const groupController = require("../controllers/groupController");
-const { checkUser } = require("../middleware/authMiddleware");
 
 const router = Router();
 
 router.post("/group", groupController.group_post);
-router.get("/group/:groupId", checkUser, groupController.group_get);
+router.put("/add-member/:groupId", groupController.add_member);
+router.get("/group/:groupId", groupController.group_get);
 router.post("/message", groupController.message_post);
 
 module.exports = router;
