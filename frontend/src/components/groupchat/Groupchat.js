2d5fba99582126d10d8258c13c1367978cbcb0df
Shubham <shubhamchadokar36@gmail.com>
2023-09-05 22:22:00 +0530

    group change

--- frontend/src/components/groupchat/Groupchat.js
+++ frontend/src/components/groupchat/Groupchat.js
@@ -1,24 +1,19 @@
 import { Dialog } from "@mui/material";
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Groups } from "../redux/actions/Action";
 import "./groupchat.css";
 import Group from "./Group";
+import { setData, setGroup } from "../redux/actions/Action";
+import axios from "axios";
 
 function Groupchat({ socket }) {
-  const [groupName, setGroupName] = useState("");
-  const [dialog, setDialog] = useState(false);
   const [currSocket, setCurrentSocket] = useState(socket);
-  const [userInfo, setUserInfo] = useState(
-    JSON.parse(localStorage.getItem("userInfo"))
-  );
-  const [image, setImage] = useState(
-    "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
-  );
-  const [groups, setgroups] = useState([]);
+  const [dialog, setDialog] = useState(false);
+  const [dialogErr, setDialogErr] = useState("");
+  const [memberEmail, setMemberEmail] = useState("");
 
   const dispatch = useDispatch();
+  const group = useSelector((state) => state.GroupManager?.group);
   // useEffect(() => {
   //   dispatch(Groups(userInfo.groupIds));
   //   console.log("hi");
@@ -28,9 +23,45 @@ function Groupchat({ socket }) {
     setCurrentSocket(socket);
   }, [socket]);
 
-  const [base64, setBase64] = useState();
+  const userData = useSelector((state) => state.UserReducers);
 
-  const submithandler = async (e) => {
+  // const submithandler = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     const config = {
+  //       headers: {
+  //         "Content-type": "application/json",
+  //       },
+  //     };
+  //     const { data } = await axios.post(
+  //       "/g/group",
+  //       {
+  //         groupDetails: {
+  //           groupName,
+  //           groupAdmin: userInfo._id,
+  //           groupImage: base64,
+  //         },
+  //         userId: userInfo._id,
+  //       },
+  //       config
+  //     );
+  //     console.log(data);
+  //     dispatch(setData(data.user));
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+
+  const groupHandle = async (id) => {
+    try {
+      const { data } = await axios.get(`/g/group/${id}`);
+      console.log(data);
+      dispatch(setGroup(data));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const addMember = async (e) => {
     e.preventDefault();
     try {
       const config = {
@@ -38,43 +69,21 @@ function Groupchat({ socket }) {
           "Content-type": "application/json",
         },
       };
-      const { data } = await axios.post(
-        "/g/group",
+      const { data } = await axios.put(
+        `/g/add-member/${group._id}`,
         {
-          groupDetails: {
-            groupName,
-            groupAdmin: userInfo._id,
-            groupMembers: [userInfo._id],
-            groupImage: base64,
-          },
-          userid: userInfo._id,
+          email: memberEmail,
         },
         config
       );
       console.log(data);
-      setgroups(data.group);
+      dispatch(setData(data.user));
+      dispatch(setGroup(data.group));
+      setDialog(false);
     } catch (err) {
       console.log(err);
+      setDialogErr(err?.response?.data?.error);
     }
-  };
-
-  const handleImage = async (e) => {
-    setImage(e.target.files[0]);
-    const base = await convertToBase64(e.target.files[0]);
-    setBase64(base);
-  };
-
-  const convertToBase64 = (file) => {
-    return new Promise((resolve, reject) => {
-      const fileReader = new FileReader();
-      fileReader.readAsDataURL(file);
-      fileReader.onload = () => {
-        resolve(fileReader.result);
-      };
-      fileReader.onerror = (error) => {
-        reject(error);
-      };
-    });
   };
 
   // const sendMessage = async (e) => {
@@ -111,41 +120,45 @@ function Groupchat({ socket }) {
         fullWidth={false}
         maxWidth="xl"
         open={dialog}
-        onClose={() => setDialog(false)}
+        onClose={() => {
+          setDialog(false);
+          setDialogErr("");
+        }}
       >
-        <form className="form" onSubmit={submithandler} action="">
-          <h2>Create Group</h2>
-          <label htmlFor="groupName">Group Name</label>
+        <form className="form" onSubmit={addMember} action="">
+          <h2>Add Member</h2>
+          <label htmlFor="MemberEmail">Email</label>
           <input
-            type="groupName"
+            type="email"
             name=""
-            value={groupName}
-            onChange={(e) => setGroupName(e.target.value)}
+            value={memberEmail}
+            onChange={(e) => {
+              setMemberEmail(e.target.value);
+              setDialogErr("");
+            }}
           />
-          <div className="email error"></div>
-          <label htmlFor="groupImage">Group Image</label>
-          <input
-            type="file"
-            name=""
-            id="groupImage"
-            accept="image/*"
-            onChange={handleImage}
-          />
-          <button>Log up</button>
+          <div className="email error">{dialogErr}</div>
+          <button>Add</button>
         </form>
       </Dialog>
       <div className="groupchat">
         <div className="group-list-container">
-          <div className="group-list-box">
-            <img
-              className="chat-profile-img"
-              src="https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
-              alt=""
-            />
-            <h1>Group 1</h1>
-          </div>
+          {userData?.groups?.map((group, i) => (
+            <div
+              className="group-list-box"
+              onClick={() => groupHandle(group?.groupId)}
+              key={group?._id}
+            >
+              <img
+                className="chat-profile-img"
+                src="https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
+                alt=""
+              />
+              <h3>{group?.groupName}</h3>
+            </div>
+          ))}
         </div>
-        <Group setDialog={setDialog} socket={currSocket} />
+        {group && <Group setDialog={setDialog} socket={currSocket} />}
       </div>
     </>
   );
