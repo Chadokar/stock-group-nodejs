95f7198af4d9744b4abbb1803cea006e8d669e75
Shubham <shubhamchadokar36@gmail.com>
2023-09-05 22:10:02 +0530

    group controll updated

--- backend/controllers/groupController.js
+++ backend/controllers/groupController.js
@@ -2,14 +2,27 @@ const Group = require("../models/GroupchatModel");
 const User = require("../models/User");
 
 module.exports.group_post = async (req, res) => {
-  const { groupDetails, userid } = req.body;
+  const { groupDetails, userId } = req.body;
   try {
-    const group = await Group.create({ groupDetails });
-    let user = await User.findById(userid);
-    await User.findByIdAndUpdate(userid, {
-      groupIds: [...user.groupIds, group._id],
+    let user = await User.findById(userId);
+    const group = await Group.create({
+      groupDetails: {
+        ...groupDetails,
+        groupMembers: [
+          {
+            memberId: userId,
+            memberName: user.firstname + " " + user.lastname,
+          },
+        ],
+      },
     });
-    user = await User.findById(userid);
+    await User.findByIdAndUpdate(userId, {
+      groups: [
+        ...user.groups,
+        { groupId: group._id, groupName: group.groupDetails.groupName },
+      ],
+    });
+    user = await User.findById(userId);
     // console.log("group: ", group, "user: ", user);
     res.status(201).json({ group, user });
   } catch (err) {
@@ -39,6 +52,53 @@ module.exports.message_post = async (req, res) => {
     const group = await Group.findById(groupId);
     console.log("group: ", group);
     res.status(201).json(group);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+module.exports.add_member = async (req, res) => {
+  const groupId = req.params.groupId;
+  const { email } = req.body;
+  try {
+    let group = await Group.findById(groupId);
+    let user = await User.findOne({ email });
+
+    // Check if the user is already a member of the group
+    const isMember = group.groupDetails.groupMembers.some(
+      (member) => member.memberId.toString() === user._id.toString()
+    );
+
+    if (isMember) {
+      return res
+        .status(400)
+        .json({ error: "User is already a member of the group." });
+    }
+    if (!user) {
+      return res.status(400).json({ error: "User not found" });
+    }
+
+    await Group.findByIdAndUpdate(groupId, {
+      groupDetails: {
+        ...group.groupDetails,
+        groupMembers: [
+          ...group.groupDetails.groupMembers,
+          {
+            memberId: user._id,
+            memberName: user.firstname + " " + user.lastname,
+          },
+        ],
+      },
+    });
+    await User.findByIdAndUpdate(user._id, {
+      groups: [
+        ...user.groups,
+        { groupId: group._id, groupName: group.groupDetails.groupName },
+      ],
+    });
+    group = await Group.findById(groupId);
+    user = await User.findById(user._id);
+    res.status(201).json({ group, user });
   } catch (err) {
     res.status(400).json(err);
   }
